91a2b0139d5e20f250b8a8a5df8aee6fb2bb28b7
Olga <baushonok@gmail.com>
2020-05-03 16:25:59 +0300

    Add child components for TabsList

---
+++ src/components/tabs-list/tab-content/tab-content.tsx
@@ -0,0 +1,13 @@
+import React, { memo, FC } from "react";
+
+import { ITabContent } from "./types";
+
+import "./tab-content.css";
+
+const TabContent: FC<ITabContent> = memo(({ data, isActive = false }) => (
+  <section className={`tab-content ${isActive ? "" : "tab-content_hidden"}`}>
+    {data}
+  </section>
+));
+
+export default TabContent;
--- src/components/tabs-list/tabs-list.tsx
+++ src/components/tabs-list/tabs-list.tsx
@@ -1,45 +1,32 @@
-import React, { useState, useCallback, FC } from "react";
+import React, { memo, useState, FC } from "react";
 
+import TabContent from "./tab-content";
+import TabToggle from "./tab-toggle";
 import { ITabsList } from "./types";
 
-import "./tabs-list.css";
-
-const TabsList: FC<ITabsList> = ({ data = [] }) => {
+const TabsList: FC<ITabsList> = memo(({ data = [] }) => {
   const [activeTabIndex, setActiveTabIndex] = useState(0);
   const isActiveTab = (tabIndex: number) => tabIndex === activeTabIndex;
 
   return (
     <section className="tabs-list">
       <ul className="tabs-list__headers">
-        {data.map(({ name }, index) => {
-          const handleClick = () => setActiveTabIndex(index);
-
-          return (
-            <button
-              className={`tab__toggle ${
-                isActiveTab(index) ? "tab__toggle_active" : ""
-              }`}
-              type="button"
-              onClick={handleClick}
-            >
-              {name}
-            </button>
-          );
-        })}
+        {data.map(({ name }, index) => (
+          <TabToggle
+            name={name}
+            index={index}
+            isActive={isActiveTab(index)}
+            onClick={setActiveTabIndex}
+          />
+        ))}
       </ul>
       <ul className="tabs-list__content">
         {data.map(({ content }, index) => (
-          <section
-            className={`tab__content ${
-              isActiveTab(index) ? "" : "tab__content_hidden"
-            }`}
-          >
-            {content}
-          </section>
+          <TabContent data={content} isActive={isActiveTab(index)} />
         ))}
       </ul>
     </section>
   );
-};
+});
 
 export default TabsList;
