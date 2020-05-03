b0b4e27cc01859da94ccd0196446653bd3869f33
Olga <baushonok@gmail.com>
2020-05-03 18:03:15 +0300

    Update tests for app

--- .hermione.conf.js
+++ .hermione.conf.js
@@ -2,17 +2,51 @@ module.exports = {
   baseUrl: "http://localhost:12345/",
 
   sets: {
-    desktop: {
-      files: "**/tests/hermione/*.hermione.js"
-    }
+    app: {
+      files: "src/tests/hermione/*.hermione.js",
+    },
+    storybook: {
+      files: "src/components/**/tests/hermione/*.hermione.js",
+    },
   },
 
   browsers: {
     chrome: {
       desiredCapabilities: {
-        browserName: "chrome"
-      }
-    }
+        browserName: "chrome",
+      },
+      windowSize: {
+        width: 1200,
+        height: 1000,
+      },
+    },
+    chromeMobile: {
+      desiredCapabilities: {
+        browserName: "chrome",
+        chromeOptions: {
+          args: ["--headless"],
+        },
+      },
+      windowSize: {
+        width: 414,
+        height: 800,
+      },
+    },
+    safariXL: {
+      desiredCapabilities: {
+        browserName: "safari",
+      },
+      windowSize: "1300x900",
+    },
+    safariMobile: {
+      desiredCapabilities: {
+        browserName: "safari",
+      },
+      windowSize: {
+        width: 414,
+        height: 700,
+      },
+    },
   },
 
   plugins: {
@@ -25,9 +59,9 @@ module.exports = {
         "Parameter .* must be a string",
         {
           name: "Cannot read property of undefined",
-          pattern: "Cannot read property .* of undefined"
-        }
-      ]
-    }
-  }
+          pattern: "Cannot read property .* of undefined",
+        },
+      ],
+    },
+  },
 };
