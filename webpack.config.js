348fa11a29eda8709c7ab50a9ed8e0c16ef71bfd
Olga <baushonok@gmail.com>
2020-05-04 18:23:35 +0300

    Update loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,56 @@
 const path = require("path");
 
-const StorytestsWebpackPlugin = require('storytests-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const StorytestsWebpackPlugin = require("storytests-webpack-plugin");
 const {
   componentNamePattern,
   storyFilesPath,
   testFilePostfixes,
   storyNamePattern,
-  testDirectoryPath
+  testDirectoryPath,
 } = require("./src/constants");
 const testTemplate = require("./src/testTemplate");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/index.tsx",
+  context: __dirname,
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js"
+    filename: "bundle.js",
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.jsx?$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      }
-    ]
+        loader: "babel-loader",
+      },
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+        options: {
+          transpileOnly: true,
+        },
+      },
+      {
+        test: /\.css$/i,
+        loader: "css-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
   },
   plugins: [
+    new ForkTsCheckerWebpackPlugin(),
     new StorytestsWebpackPlugin({
       storyFilesPath,
       componentNamePattern,
       storyNamePattern,
       testDirectoryPath,
       testFilePostfixes,
-      testTemplate
-    })
-  ]
+      testTemplate,
+    }),
+  ],
 };
