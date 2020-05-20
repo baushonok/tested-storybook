280bf532633baeb2c4cf305b533d6554cc2258ff
Olga <baushonok@gmail.com>
2020-05-20 19:04:43 +0300

    Add correctly generated test

---
+++ src/components/accordion/tests/hermione/With-remove-control.hermione.js
@@ -0,0 +1,9 @@
+describe('Accordion', function() {
+    it('With-remove-control', function() {
+        const selector = '.story-container';
+
+        return this.browser
+            .url('iframe.html?selectedKind=Accordion&selectedStory=With-remove-control')
+            .assertView('With-remove-control', selector);
+    });
+});
