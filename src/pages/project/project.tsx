db31772cfea53d68bda4898c7519f662a4b22d14
Olga <baushonok@gmail.com>
2020-05-23 13:09:03 +0300

    Кнопка добавлена в приложение

--- src/pages/project/project.tsx
+++ src/pages/project/project.tsx
@@ -1,10 +1,18 @@
 import React, { FC } from "react";
 
 import { IProjectPage } from "./types";
+import Button from "../../components/button";
 import TabsList from "../../components/tabs-list";
 
+import './project.css';
+
 const ProjectPage: FC<IProjectPage> = ({ data = [] }) => {
-  return <main>{<TabsList data={data} />}</main>;
+  return (
+    <main className="project">
+      <TabsList data={data} />
+      <Button className="project__feedback">Send feedback</Button>
+    </main>
+  );
 };
 
 export default ProjectPage;
