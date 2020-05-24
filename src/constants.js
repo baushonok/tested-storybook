837c4573e0a171e3b0b87a7580e8a8cde9e6ec38
Olga <baushonok@gmail.com>
2020-05-24 11:26:14 +0300

    Update tests

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
-const componentNamePattern = /[a-z]+(?=", module)/gi;
-const storyNamePattern = /[a-z ]+(?=", \(\) => )/gi;
+const componentNamePattern = /[a-z]+(?=', module)/gi;
+const storyNamePattern = /[a-z ]+(?=', \(\) => )/gi;
 
 const storyFilesPath = path.resolve(
   __dirname,
